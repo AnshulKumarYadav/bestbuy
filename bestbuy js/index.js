15af1f8a11decc2881b9c652f8b10d2e36397503
AnshulKumarYadav <rishabhyadav3602@gmail.com>
2022-03-31 11:10:49 +0530

    indexadded

---
+++ bestbuy js/index.js
@@ -0,0 +1,17 @@
+document.querySelector("#canada").addEventListener("click",canadaHome);
+document.querySelector("#us").addEventListener("click",usHome);
+document.querySelector("#mexico").addEventListener("click",mexicoHome);
+
+
+function canadaHome()
+{
+    window.location.href="/besbuy HTML/home.html";
+}
+
+function usHome(){
+    window.location.href="/besbuy HTML/home.html";
+}
+function mexicoHome()
+{
+    window.location.href="/besbuy HTML/home.html";
+} 
